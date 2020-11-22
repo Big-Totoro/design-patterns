bee272b9dc5a0e9d7b4066dcd52351a9d24dbc09
sskuratov <s.a.skuratov@gmail.com>
2020-11-22 09:07:37 +0600

    Restructured

--- visitor/src/main/java/io/sskuratov/visitor/Shape.java
+++
@@ -1,6 +0,0 @@
-package io.sskuratov.visitor;
-
-public interface Shape {
-
-    void visit(Visitor visitor);
-}
--- visitor/src/main/java/io/sskuratov/visitor/Visitor.java
+++
@@ -1,9 +0,0 @@
-package io.sskuratov.visitor;
-
-public interface Visitor {
-
-    void visit(Circle circle);
-    void visit(Rectangle rectangle);
-    void visit(Triangle triangle);
-    void visit(Square square);
-}
